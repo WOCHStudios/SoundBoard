784fc998054cb247358e6a4c9d91df18bd9d3554
Dave McPherson <dmcp89@gmail.com>
2015-05-07 18:32:19 -0400

    Import into Android studio
    Adding gradle files

--- gen/com/wochstudios/theleaguesoundboard/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.wochstudios.theleaguesoundboard;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
