2b5a432145bc62ec45a5169454e891fcca347e09
Dave McPherson <dmcp89@gmail.com>
2015-06-01 19:35:20 -0400

    failed

---
+++ src/com/wochstudios/soundboard/utils/SoundPlayer.java
@@ -0,0 +1,30 @@
+package com.wochstudios.soundboard.utils;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.net.*;
+
+public class SoundPlayer {
+	private MediaPlayer mp;
+	
+	public SoundPlayer(){
+		mp = new MediaPlayer();
+	}
+	
+	public boolean playSound(Context con, Uri uri){
+		try {
+			if(!mp.isPlaying()){
+				mp = MediaPlayer.create(con, uri);
+				mp.start();
+				return true;
+			}
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+			return false;
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return false;
+	}
+}
