76b7bacd53d827529636522704ad5faa6c90e345
Dave McPherson <dmcp89@gmail.com>
2015-06-01 19:35:25 -0400

    Revert "failed"
    This reverts commit 2b5a432145bc62ec45a5169454e891fcca347e09.

--- src/com/wochstudios/soundboard/Interfaces/AddSoundDialogListener.java
+++
@@ -1,8 +0,0 @@
-package com.wochstudios.soundboard.Interfaces;
-import android.app.*;
-
-public interface AddSoundDialogListener
-{
-	public void onDialogPositiveClick(DialogFragment dialog);
-	public void onDialogNegativeClick(DialogFragment dialog);
-}
